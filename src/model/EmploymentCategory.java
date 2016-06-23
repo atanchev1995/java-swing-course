3dd017fab66730ca4fb73a2dab33ff19bd0cd7fd
Aleksandar Tanchev <a.tanchev1995@gmail.com>
2016-06-23 21:47:45 +0200

    java-swing-course

---
+++ src/model/EmploymentCategory.java
@@ -0,0 +1,16 @@
+package model;
+
+public enum EmploymentCategory {
+	employed("employed"), selfEmployed("self employed"), unemployed("unemployed"), other("other");
+
+	private String text;
+
+	private EmploymentCategory(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return text;
+	}
+}
